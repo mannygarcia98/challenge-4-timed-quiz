38cee230d32b75a2a4b1ca2356f8938403167302
Manny Garcia <mannysg88@gmail.com>
2022-03-06 16:23:01 -0600

    added array for questions and event listener on button

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,3 +1,39 @@
-//I need to figure out a way to have all the questions on one webpage. It is suggested that we have all the questions stored in an array and loop through that to display the questions one after another.
+//need an array for questions
+var questions = [
+  {
+    question: "Which of the following can be used to redirect a url using JavaScript?",
+    choices: ["document.location =", "browser.location =", "navigator.location =", "window.location ="],
+    answer: "window.location =",
+  },
+  {
+    question: "Which of the following type of variable is visible only within a function where it is defined?",
+    choices: ["global variable", "local variable", "Both of the above.", "None of the above."],
+    answer: "local variable",
+  },
+  {
+    question: "Which built-in method returns the calling string value converted to lower case?",
+    choices: ["toLowerCase()", "toLower()", "changeCase(case)", "None of the above."],
+    answer: "Both",
+  },
+  {
+    question: "Which of the following can be used to return a Number's value?",
+    choices: ["toString();", "valueOf();", "toLocaleString();", "toPrecision();"],
+    answer: "valueOf();",
+  },
+  {
+    question: "Which of the following is not a valid JavaScript variable name?",
+    choices: ["2names", "_first_and_last_names", "FirstAndLast", "None of the above"],
+    answer: "2names",
+  },
+];
 
-//when user clicks on any button -->
+function quiz() {
+  console.log(questions);
+}
+//event listener which starts the quiz
+var startBtn = document.querySelector("#start-quiz");
+startBtn.onclick = quiz;
+
+//create a countdown timer
+
+//variables which target html elements
