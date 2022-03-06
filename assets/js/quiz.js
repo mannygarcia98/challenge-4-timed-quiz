6e51dd7f8082919fa7b4a3976f6048ee9d2003a6
Manny Garcia <mannysg88@gmail.com>
2022-03-06 16:40:52 -0600

    added countdown timer

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -27,12 +27,48 @@ var questions = [
   },
 ];
 
+var seconds = 60;
+var timer;
+function myFunction() {
+  if (seconds < 60) {
+    // I want it to say 1:00, not 60
+    document.querySelector("#time-remaining").innerHTML = seconds;
+  }
+  if (seconds > 0) {
+    // so it doesn't go to -1
+    seconds--;
+  } else {
+    clearInterval(timer);
+  }
+}
+document.querySelector("#time-remaining").onclick = function () {
+  if (!timer) {
+    timer = window.setInterval(function () {
+      myFunction();
+    }, 1000); // every second
+  }
+};
+
 function quiz() {
-  console.log(questions);
+  if (seconds < 60) {
+    // I want it to say 1:00, not 60
+    document.querySelector("#time-remaining").innerHTML = seconds;
+  }
+  if (seconds > 0) {
+    // so it doesn't go to -1
+    seconds--;
+  } else {
+    clearInterval(timer);
+  }
+  if (!timer) {
+    timer = window.setInterval(function () {
+      myFunction();
+    }, 1000); // every second
+  }
 }
 //event listener which starts the quiz
 var startBtn = document.querySelector("#start-quiz");
-startBtn.onclick = quiz;
+startBtn.addEventListener("click", quiz);
 
 //create a countdown timer
 
