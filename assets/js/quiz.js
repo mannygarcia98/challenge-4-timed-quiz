f10b036c0cb851286f3d41c1bcca2d686d643d7f
Manny Garcia <mannysg88@gmail.com>
2022-03-06 15:19:19 -0600

    completed creating html sections

---
+++ assets/js/quiz.js
@@ -0,0 +1,3 @@
+//I need to figure out a way to have all the questions on one webpage. It is suggested that we have all the questions stored in an array and loop through that to display the questions one after another.
+
+//when user clicks on any button -->
